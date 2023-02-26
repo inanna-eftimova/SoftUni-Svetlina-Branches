96a35a602bdc8812762d0f54888a9a84945a7590
inanna-eftimova <eftimovainana@gmail.com>
2023-02-26 19:13:17 +0200

    Added resources

---
+++ solution.js
@@ -0,0 +1,31 @@
+let reservation =
+{
+    startDate: null,
+    endDate: null,
+    guestsCount: 0,
+    roomType: null,
+    name: null,
+    phone: null,
+    email: null
+}
+
+function changeContent(className) {
+    document.querySelectorAll('.custom-form').forEach(div => div.classList.add('hidden'));
+    document.querySelector(`.${className}`).classList.remove('hidden');
+}
+
+document.querySelector('#new-reservation').addEventListener('click', (e) => cleanData(e));
+
+function cleanData(e) {
+    changeContent('search-form-content');
+}
+
+document.querySelector('#search-back-btn').addEventListener('click', (e) => fillSearchForm(e));
+
+function fillSearchForm(e) {
+    e.preventDefault();
+    changeContent('search-form-content');
+    document.querySelector('#check-in').value = reservation.startDate;
+    document.querySelector('#check-out').value = reservation.endDate;
+    document.querySelector('#people').value = reservation.guestsCount;
+}
